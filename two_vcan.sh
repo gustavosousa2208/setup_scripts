e03a773c75fd3163c455b1e64e546d62d74b533f
Gustavo Sousa <gustavosousa@alu.ufc.br>
2023-10-07 18:24:14 -0300

    sets interfaces up

--- two_vcan.sh
+++ two_vcan.sh
@@ -4,10 +4,11 @@ sudo apt install can-utils
 sudo modprobe vcan
 sudo modprobe can-gw
 
-sudo ip link add vcan0 type vcan
-sudo ip link add vcan1 type vcan
+sudo ip link add dev vcan0 type vcan
+sudo ip link add dev vcan1 type vcan
 
 sudo cangw -A -s vcan0 -d vcan1 -e
 sudo cangw -A -s vcan1 -d vcan0 -e
 
-
+sudo ip link set vcan0 up
+sudo ip link set vcan1 up
